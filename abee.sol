cbad71a222e174d4d55a44f628229cf2857647a4
abeefoundationorg <42715450+abeefoundationorg@users.noreply.github.com>
2018-08-26 08:18:13 -0500

    Update abee.sol

--- abee.sol
+++ abee.sol
@@ -1,1 +1,23 @@
+pragma solidity ^0.4.24;
 
+import "https://github.com/dappmind/openzeppelin-solidity/contracts/token/ERC20/StandardBurnableToken.sol";
+
+
+contract XYZToken is StandardBurnableToken {
+
+  string public constant name = "ABEE";
+  string public constant symbol = "ABX";
+  uint8 public constant decimals = 18;
+
+  uint256 public constant INITIAL_SUPPLY = 3000000000;
+
+  /**
+   * @dev Constructor that gives msg.sender all of existing tokens.
+   */
+  constructor() public {
+    totalSupply_ = INITIAL_SUPPLY;
+    balances[msg.sender] = INITIAL_SUPPLY;
+    emit Transfer(address(0), msg.sender, INITIAL_SUPPLY);
+  }
+
+}
